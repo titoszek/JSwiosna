3541f24466d2e3dd0ca07c85b7f59f72c2f1561f
titoszek <113134176+titoszek@users.noreply.github.com>
2024-06-13 19:37:58 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,107 @@
+ function z35() {
+    window.open("z35_Dzi.html", "okienko_z35","toolbar=no, directories=no, menubar=no, height=800, width=900, top=350, left=600");
+ }
+ 
+ function z36() {
+    window.open("z36_Dzi.html", "okienko_z36","toolbar=no, directories=no, menubar=no, height=800, width=900, top=350, left=600");
+ }
+
+ function z38() {
+    window.open("z38_Dzi.html", "okienko_z38","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+ }
+ 
+ function z39() {
+    window.open("z39_Dzi.html", "okienko_z39","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+ }
+
+ function z40() {
+    window.open("z40_Dzi.html", "okienko_z40","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+ }
+
+ function z38w() {
+    window.open("z38w_Dzi.html", "okienko_z38","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+ }
+
+ function z41() {
+    window.open("z41w_Dzi.html", "okienko_z41","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+ }
+
+ function z42() {
+    window.open("z42w_Dzi.html", "okienko_z42","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+ }
+
+ function kod42() {
+    window.open("z42w_Dzi_kod.html", "okienko_kod42","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+ }
+
+ function z43() {
+    window.open("z43w_Dzi.html", "okienko_z43","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+ }
+
+ function kod43() {
+    window.open("z43w_Dzi_kod.html", "okienko_kod43","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+ }
+
+ function z44() {
+    window.open("z44w_Dzi.html", "okienko_z44","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+ }
+
+ function kod44() {
+    window.open("z44w_Dzi_kod.html", "okienko_kod44","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+ }
+
+ function z45() {
+    window.open("z45w_Dzi.html", "okienko_z45","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+ }
+
+ function kod45() {
+    window.open("z45w_Dzi_kod.html", "okienko_kod45","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+ }
+
+ function z46() {
+    window.open("z46w_Dzi.html", "okienko_z46","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+ }
+
+ function kod46() {
+    window.open("z46w_Dzi_kod.html", "okienko_kod46","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+ }
+
+ function z47() {
+    window.open("z47w_Dzi.html", "okienko_z47","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+ }
+
+ function kod47() {
+    window.open("z47w_Dzi_kod.html", "okienko_kod47","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+ }
+
+ function z50() {
+    window.open("z50w_Dzi.html", "okienko_z50","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+ }
+
+ function kod50() {
+    window.open("z50w_Dzi_kod.html", "okienko_kod50","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+ }
+
+ function z51() {
+    window.open("z51w_Dzi.html", "okienko_z51","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+ }
+
+ function z52() {
+    window.open("z52w_Dzi.html", "okienko_z52","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+ }
+
+ function z53() {
+    window.open("z53w_Dzi.html", "okienko_z53","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+ }
+
+ function z56() {
+    window.open("z56w_Dzi.html", "okienko_z56","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+ }
+
+ function z57() {
+    window.open("z57w_Dzi.html", "okienko_z57","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+ }
+
+ function z58() {
+    window.open("z58w_Dzi.html", "okienko_z58","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+ }
